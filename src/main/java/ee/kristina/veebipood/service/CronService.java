6d5110b1b182a8d0cae20d16a9adbf13686c722d
OKris <oim.kristina@gmail.com>
2026-03-03 11:00:18 +0200

    cron to update so render wouldnt crach after 15min

--- src/main/java/ee/kristina/veebipood/service/CronService.java
+++ src/main/java/ee/kristina/veebipood/service/CronService.java
@@ -2,6 +2,9 @@ package ee.kristina.veebipood.service;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CronService {
@@ -13,17 +16,17 @@ public class CronService {
     //***** kuu
     //****** nadalapaev kus nii 0 kui ka 7 on pyhap
 
-    @Scheduled(cron = "0 0 17 * * 1-5") // 17.00 every day between M-F
+    /*@Scheduled(cron = "0 0 17 * * 1-5") // 17.00 every day between M-F
     public void runOnWorkDays() {
         System.out.println("cron service is running after every second");
-    }
+    }*/
 
-    @Scheduled(cron = "*/2 30 10 * * *") // every 2 sec at 10:30
-    public void runAfterTwoSeconds() {
-        System.out.println("cron service is running after every 2 seconds");
-    }
+    //@Scheduled(cron = "*/2 30 10 * * *") // every 2 sec at 10:30
+    //public void runAfterTwoSeconds() {
+        //System.out.println("cron service is running after every 2 seconds");
+    //}
 
-    @Scheduled(cron = "0 0 * * * *") // every hour
+    /*@Scheduled(cron = "0 0 * * * *") // every hour
     public void runEveryHour() {
         System.out.println("cron service is running every hour");
     }
@@ -33,7 +36,16 @@ public class CronService {
         System.out.println("cron service is running every hour");
         //bookingRepository.findByCreatedBetween(Homme 24h parast, homme 25h parast);
         // sms/ email booking teavitusest
-    }
+    }*/
 
     // kustuta koik maksmata tellimused nt
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Scheduled(cron = "0 */10 * * * *")
+    public void callUrlEveryTenMinutes() {
+        String url = "https://veebipood-01-2026.onrender.com/products";
+        restTemplate.getForObject(url, String.class);
+        System.out.println(LocalDateTime.now() + " - Called " + url + " -> OK");
+    }
 }
